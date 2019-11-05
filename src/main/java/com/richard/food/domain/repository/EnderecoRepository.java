4f231d4fd8e55ff5ece7d0999ee4cc702ed65ddc
Ricardo Viana <richardluizv@gmail.com>
2019-11-04 23:39:59 -0300

    mapeando classes incorporáveis com @Embedded e @Embeddable

--- src/main/java/com/richard/food/domain/repository/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.richard.food.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.richard.food.domain.model.Endereco;
-
-public interface EnderecoRepository extends JpaRepository<Endereco, Long>{
-
-}
