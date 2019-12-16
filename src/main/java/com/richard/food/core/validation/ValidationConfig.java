449721e92dc6766394e88da1c8db84228e3b4725
Ricardo Viana <richardluizv@gmail.com>
2019-12-16 00:20:46 -0300

    Usando o Resource Bundle do Spring como Resource Bundle do Bean Validation

---
+++ src/main/java/com/richard/food/core/validation/ValidationConfig.java
@@ -0,0 +1,18 @@
+package com.richard.food.core.validation;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Component
+public class ValidationConfig {
+
+    @Bean
+    public LocalValidatorFactoryBean validator(MessageSource messageSource) {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource);
+        return  bean;
+    }
+
+}
