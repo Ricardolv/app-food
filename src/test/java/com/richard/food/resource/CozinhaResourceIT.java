6d92c3c578a8394b6a1183c8aa6f63108a9fe041
Ricardo Viana <richardluizv@gmail.com>
2019-12-16 02:47:31 -0300

    Validando o corpo da resposta HTTP

--- src/test/java/com/richard/food/resource/CozinhaResourceIT.java
+++ src/test/java/com/richard/food/resource/CozinhaResourceIT.java
@@ -1,6 +1,7 @@
 package com.richard.food.resource;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +32,21 @@ public class CozinhaResourceIT {
 			.get()
 		.then()
 			.statusCode(HttpStatus.OK.value());
+	}
+	
+	@Test
+	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		given()
+			.basePath("/cozinhas")
+			.port(port)
+			.accept(ContentType.JSON)
+		.when()
+			.get()
+		.then()
+			.body("", hasSize(4));
+//			.body("nome", hasItems("Indiana", "Tailandesa"));
 	}
 
 }
