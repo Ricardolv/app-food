f27398820a38193896bb87c75cd446c337a6951e
Ricardo Viana <richardluizv@gmail.com>
2020-01-26 04:15:51 -0300

    Implementando os endpoints de associação de formas de pagamento em restaurantes

--- src/main/java/com/richard/food/domain/model/Restaurante.java
+++ src/main/java/com/richard/food/domain/model/Restaurante.java
@@ -10,7 +10,9 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 @Getter
@@ -56,7 +58,7 @@ public class Restaurante implements Serializable {
 	@JoinTable(name = "restaurante_forma_pagamento",
 			   joinColumns = @JoinColumn(name = "restaurante_id"),
 			   inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
-	private List<FormaPagamento> formasPagamento = new ArrayList<>();
+	private Set<FormaPagamento> formasPagamento = new HashSet<>();
 
 	@OneToMany(mappedBy = "restaurante")
 	private List<Produto> produtos = new ArrayList<>();
